65cd1e69f5d6bbf4c24789828fbdca3b7d6b3b54
Enrico Sallusti <keridano@gmail.com>
2022-05-10 17:04:23 +0200

    feat: added Bonus enum

---
+++ app/src/main/java/com/keridano/soccersim/model/enum/Bonus.kt
@@ -0,0 +1,30 @@
+package com.keridano.soccersim.model.enum
+
+import com.keridano.soccersim.model.Match
+import com.keridano.soccersim.model.Team
+
+/**
+ * A series of [Bonus] which are representative of a particular [Team]
+ */
+enum class Bonus {
+    /**
+     * Gives 10 bonus points to [Team] strength
+     */
+    BEST_PLAYERS,
+
+    /**
+     * Removes 1 conceded Goal in the [Match] simulation
+     */
+    BEST_DEFENSE,
+
+    /**
+     * Gives 5 Bonus points to [Team] strength
+     */
+    BEST_COACH,
+
+    /**
+     * The [Match] simulation will use a (10..50) [IntRange.random] range for this [Team]
+     * instead of the default (0..40) [IntRange.random] range
+     */
+    LUCKY_TEAM
+}
